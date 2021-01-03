5b7097bbcd32e4acc5346a90a0963fdcb2adbddb
Vanilla-Eis-Creme <76595394+Vanilla-Eis-Creme@users.noreply.github.com>
2021-01-03 14:20:40 +0100

    Add files via upload

---
+++ CELSIUS TO FAHRENHEIT BOI.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+int Celc, Faren;
+
+cout << "Input the temperature in Celsius.";
+cin >> Celc;
+
+Faren = (Celc * 9/5) + 32;
+cout << "This is the temperature in Fahrenheit:" << Faren << "F";
+
+
+
+
+
+
+
+
+
+return 0;
+}
+
